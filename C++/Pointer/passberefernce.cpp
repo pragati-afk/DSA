ac3a328ccecd98fd94585f6a8adf3c2305344fd3
Pragati Upadhyay <pragatiupadhyayy@gmail.com>
2023-12-06 00:57:41 +0530

    file upload commit

--- C++/Pointer/passberefernce.cpp
+++ C++/Pointer/passberefernce.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 using namespace std;
+void swap(int *a, int *b){
+    int temp;
+    temp=*b;
+    *b=*a;
+    *a=temp;
+}
 int main(){
-    
+    int a=2;
+    int b=4;
+    swap(&a,&b);
+    cout<<a<<b;
+
 }
