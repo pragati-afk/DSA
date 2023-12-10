37baf4268d46ee9364c582d6b3352362faecdc85
Pragati Upadhyay <pragatiupadhyayy@gmail.com>
2023-12-11 03:02:47 +0530

    file upload

---
+++ C++/String/string.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+  
+     string str1;
+    
+  
+    getline(cin, str1);
+    
+    cout<<str1<<endl;
+}
+/*getline se poori line aati hai
+cin se ek word aajata hai*/
